c4b12030805b13c8e4e5e448ff33575400a4a0f5
VitulakCZ <stemberk.vit@gmail.com>
2025-04-17 06:03:25 +0200

    Nový Leetcode problém

---
+++ leetcode/count_equal_and_div_pairs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int count_pairs(vector<int>& nums, int k) {
+    int pocet = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = nums.size() - 1; j > i; j--) {
+            if (nums[i] == nums[j] && (i * j) % k == 0) pocet++;
+        }
+    }
+    return pocet;
+}
+
+int main() {
+    vector<int> nums = {3,1,2,2,2,1,3};
+    cout << count_pairs(nums, 2);
+    return 0;
+}
